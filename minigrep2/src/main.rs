77203ab84a95853c6be171ec47d86b908f494ddc
rapidshaurya <103172716+rapidshaurya@users.noreply.github.com>
2022-04-15 18:59:39 +0530

    Add files via upload

---
+++ minigrep2/src/main.rs
@@ -0,0 +1,21 @@
+use std::env;
+use std::process;
+
+use minigrep2::Config;
+
+fn main() {
+    
+
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    // --snip--
+
+    if let Err(e) = minigrep2::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
